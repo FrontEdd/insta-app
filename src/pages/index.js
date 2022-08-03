9383f72449776fccfea52e093e97dc27cad9b82d
FrontEdd <edgar@urbaner.com>
2022-08-02 20:49:39 -0500

    Agregamos interface de búsqueda users y de entrada a perfil user

---
+++ src/pages/index.js
@@ -0,0 +1,6 @@
+// * Estamos exportando Search pero con el alias "SearchView"
+// * Al poner un alias cuando importemos esta vista usaremos ese mismos alias
+
+export { default as SearchView } from "./Search"
+export { default as UsersView } from "./Users"
+export { default as UserView } from "./User"
--- src/services/index.js
+++ src/services/index.js
@@ -1,13 +1,28 @@
 import { config } from "./config";
 
-/**
+/*
  * Esta funcion consume el api de github y retorna una lista de usuarios
- */
+*/
 export const searchUsers = async (username) => {
   try {
     const response = await fetch(
       `${config.baseUrl}/search/users?q=${username}`
     );
+
+    const data = await response.json();
+
+    return data;
+  } catch (error) {
+    return error;
+  }
+};
+
+/*
+ * Esta funcion va a traer el detalle de cada usuario
+*/
+export const searchUser = async (username) => {
+  try {
+    const response = await fetch(`${config.baseUrl}/users/${username}`);
 
     const data = await response.json();
 
